21f1ada2a6e5ec87a46f9aa2ce5ecd4b280e5d9f
Jelte Dirks <j.dirks@outlook.com>
2024-06-22 19:59:01 +0200

    read file content and split up by lines
    Reads the file using an intermediary buffer and split it up by lines.
    The lines are echoed and the program exits.
    
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- src/main.c
+++ src/main.c
@@ -6,14 +6,15 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/stat.h>
 #include <unistd.h>
 
 #define READ_BUFFER_SIZE 1024
+#define MAX_LENGTH 200
 
 int main(int argc, char **argv)
 {
-
   char file_path[MAX_ARG_LEN];
 
   if (handle_args(argc, argv, file_path)) {
@@ -34,6 +35,7 @@ int main(int argc, char **argv)
   }
 
   char read_buf[READ_BUFFER_SIZE];
+  char word_buf[MAX_LENGTH];
   ssize_t bytes_read;
   trie_t *triep = malloc(sizeof(trie_t));
 
@@ -41,8 +43,51 @@ int main(int argc, char **argv)
     fprintf(stderr, "could not build tree, check stderr for the reason\n");
   }
 
+  unsigned int word_offset;
+  int line_start_offset;
+  size_t length;
+  unsigned int i;
+
   while ((bytes_read = read(fd, read_buf, READ_BUFFER_SIZE)) > 0) {
-    printf("READ: %s\n", read_buf);
+    line_start_offset = -1;
+    i = 0;
+
+    while (i < bytes_read) {
+      char c = read_buf[i];
+      switch (c) {
+        case '\n':
+          length = i - line_start_offset - 1;
+
+          if (length > (MAX_LENGTH - 1)) {
+            fprintf(stderr, "length of the next line (=%ld) exceeds max length (=%d)\n",
+                    length,
+                    MAX_LENGTH);
+            return 1;
+          }
+
+          if (word_offset > 0) {
+            memcpy(word_buf + (word_offset - 1) * sizeof(char),
+                   read_buf + line_start_offset + 1,
+                   length * sizeof(char));
+            word_buf[word_offset + length - 1] = '\0';
+          } else {
+            memcpy(word_buf, read_buf + line_start_offset + 1,
+                   length * sizeof(char));
+            word_buf[length] = '\0';
+          }
+
+          printf("%s\n", word_buf);
+
+          line_start_offset = i;
+          word_offset = 0;
+          break;
+      }
+
+      ++i;
+    }
+
+    word_offset = i - line_start_offset;
+    memcpy(word_buf, read_buf + line_start_offset + 1, word_offset * sizeof(char));
   };
 
   destroy_trie(triep);
