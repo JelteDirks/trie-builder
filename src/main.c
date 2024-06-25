c53601f277676c9449769702d253a11855a1dfd4
Jelte Dirks <j.dirks@outlook.com>
2024-06-25 22:15:34 +0200

    save test fn here
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- src/main.c
+++ src/main.c
@@ -10,6 +10,8 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
+void testfn();
+
 int main(int argc, char **argv)
 {
   char file_path[MAX_ARG_LEN];
@@ -88,5 +90,64 @@ int main(int argc, char **argv)
     memcpy(word_buf, read_buf + line_start_offset + 1, word_offset * sizeof(char));
   };
 
+  trie_print(triep);
   trie_destroy(triep);
+}
+
+void testfn() {
+
+  trie_t *const trie1 = malloc(sizeof(trie_t));
+  trie_t *const trie2 = malloc(sizeof(trie_t));
+
+  if (init_trie(trie1)) {
+    fprintf(stderr, "could not build tree, check stderr for the reason\n");
+  }
+
+  if (init_trie(trie2)) {
+    fprintf(stderr, "could not build tree, check stderr for the reason\n");
+  }
+
+  trie_add_value(trie2, "D", 1);
+  trie_add_value(trie2, "DE", 2);
+  trie_add_value(trie2, "DA", 2);
+  trie_add_value(trie2, "DA", 2);
+
+  trie1->root->children = malloc(sizeof(trie_node_t));
+  trie1->root->n_children = 1;
+
+  trie_node_t *const firstchild = &trie1->root->children[0];
+
+  firstchild->key = 'D';
+  firstchild->children = malloc(2 * sizeof(trie_node_t));
+  firstchild->n_children = 2;
+  firstchild->entries = malloc(sizeof(trie_entry_t));
+  firstchild->n_entries = 1;
+  firstchild->entries->value = "D";
+
+  trie_node_t *const firstfirstchild = &firstchild->children[0];
+
+  firstfirstchild->key = 'E';
+  firstfirstchild->children = NULL;
+  firstfirstchild->n_children = 0;
+  firstfirstchild->entries = malloc(sizeof(trie_entry_t));
+  firstfirstchild->n_entries = 1;
+  firstfirstchild->entries->value = "DE";
+
+  trie_node_t *const secondfirstchild = &firstchild->children[1];
+
+  secondfirstchild->key = 'A';
+  secondfirstchild->children = NULL;
+  secondfirstchild->n_children = 0;
+  secondfirstchild->entries = malloc(sizeof(trie_entry_t));
+  secondfirstchild->n_entries = 2;
+  secondfirstchild->entries->value = "DA";
+
+  secondfirstchild->entries->next = malloc(sizeof(trie_entry_t));
+  secondfirstchild->entries->next->value = "DX";
+
+  trie_print(trie1);
+  trie_print(trie2);
+
+  trie_destroy(trie1);
+  trie_destroy(trie2);
 }
