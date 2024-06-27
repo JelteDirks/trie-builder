466aebdc8cb03f8cfbada0fbccc502b27469fa61
Jelte Dirks <j.dirks@outlook.com>
2024-06-27 23:15:15 +0200

    add without errors on small files
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- src/trie.c
+++ src/trie.c
@@ -49,9 +49,104 @@ int init_trie(trie_t *const trie)
   return 0;
 }
 
-int trie_add_value(trie_t *const t, char *const v, unsigned int v_len)
+int increase_children_arr(trie_node_t *const node)
 {
-  printf("adding value to trie: %s\n", v);
+  trie_node_t *newarr = realloc(node->children, sizeof(trie_node_t) * node->n_children + 1);
+  if (newarr == NULL) return 1;
+  init_trie_node(&newarr->children[node->n_children++]);
+  node->children = newarr;
   return 0;
 }
 
+trie_node_t* get_last_child(trie_node_t *const node)
+{
+  if (node->n_children == 0 || node->children == NULL) {
+    return NULL;
+  }
+  return &node->children[node->n_children - 1];
+}
+
+int append_empty_child(trie_node_t *const node)
+{
+  if (node->children == NULL || node->n_children == 0) {
+    trie_node_t *newnode = create_empty_node();
+    if (newnode == NULL) return 1;
+    node->children = newnode;
+    node->n_children = 1;
+    return 0;
+  }
+  return increase_children_arr(node);
+}
+
+trie_node_t* append_child_with_key(trie_node_t *const node, char key)
+{
+  if (append_empty_child(node)) {
+    return NULL;
+  }
+
+  trie_node_t* lastchild = get_last_child(node);
+  lastchild->key = key;
+
+  return lastchild;
+}
+
+int append_new_chain(trie_node_t *const base, char *const value, unsigned int start, unsigned int length)
+{
+  unsigned int pos = start;
+  trie_node_t *cur = base;
+
+  while (pos < length) {
+    trie_node_t* newchild = append_child_with_key(cur, value[pos]);
+    if (newchild == NULL) {
+      return 1;
+    }
+    cur = newchild;
+    pos++;
+  }
+
+  size_t len = strnlen(value, MAX_LENGTH);
+  if (len > MAX_LENGTH) {
+    fprintf(stderr, "length exceeds max length: %s\n", value);
+    return 1;
+  }
+  cur->value = (char *) malloc(sizeof(char) * (len + 1));
+  strncpy(cur->value, value, len);
+  cur->value[len] = '\0';
+
+  return 0;
+}
+
+int trie_add_value(trie_t *const t, char *const v, unsigned int v_len)
+{
+  printf("adding value to trie: %s\n", v);
+
+  trie_node_t * cur = t->root;
+  unsigned int pos = 0;
+
+  while (pos < v_len) {
+    if (cur->children == NULL || cur->n_children == 0) {
+      return append_new_chain(cur, v, pos, v_len);
+    } else {
+      trie_node_t *found = NULL;
+      for (int i = 0; i < cur->n_children; i++) {
+        trie_node_t *child = &cur->children[i];
+        if (child->key == v[pos]) {
+          found = child;
+        }
+      }
+      if (found == NULL) {
+        return append_new_chain(cur, v, pos, v_len);
+      } else {
+        cur = found;
+        pos++;
+      }
+    }
+  }
+
+  return 0;
+}
+
+void trie_destroy(trie_t *const trie)
+{
+  // TODO: clean up all memory from this trie.
+}
