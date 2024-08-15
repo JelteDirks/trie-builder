a382735d9ab49ce8e3247c4fbd509e3b15076550
Jelte Dirks <j.dirks@outlook.com>
2024-08-15 16:52:13 +0200

    consider buffer overflow case and handle trailing chars
    The trailing characters in one buffer read are now copied to the front
    of the buffer and the new characters are appended to the characters from
    last iteration. Possibly push up the vector operations that are not
    required anymore since they are already processed.
    
    Buffer overflow happens when lines are longer than the buffer, which can
    never be processed correctly. Throw an error in this case since it's
    unrecoverable.
    
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- src/main.c
+++ src/main.c
@@ -4,13 +4,16 @@
 #include "trie.h"
 #include <errno.h>
 #include <fcntl.h>
+#include <string.h>
 #include <stdio.h>
 #include <stdint.h>
-#include <string.h>
 #include <sys/stat.h>
 #include <unistd.h>
 #include <arm_neon.h>
 
+#define ELINES_TOO_LONG 1
+#define EFILE_CLOSE 2
+#define EFILE_OPEN 3
 
 static const uint8x16_t newlines = {10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10};
 
@@ -36,8 +39,6 @@ int main(int argc, char **argv)
     return 1;
   }
 
-  printf("Analyzing file: %s\n", file_path);
-
   int fd = open(file_path, O_RDONLY);
   if (fd == -1) {
     fprintf(stderr, "error opening file %s\n", file_path);
@@ -46,7 +47,7 @@ int main(int argc, char **argv)
       printf("access denied\n");
       break;
     }
-    return 1;
+    return EFILE_OPEN;
   }
 
   char read_buf[READ_BUFFER_SIZE];
@@ -69,81 +70,61 @@ int main(int argc, char **argv)
     return 1;
   }
 
+  int read_offset = 0;
 
-  // TODO: use newline markers to copy into word buffer
-  // NOTE: maybe use the existing buffer for the string copy? Replace the newline
-  // with a 0 byte and start the string copy right after the last known location.
-  char word_buffer[MAX_LENGTH];
+  while ((bytes_read = read(fd, &read_buf[read_offset], READ_BUFFER_SIZE - read_offset)) > 0) {
 
-  int read_buffer_offset = 0;
-  while ((bytes_read = read(fd, &read_buf[read_buffer_offset], READ_BUFFER_SIZE - read_buffer_offset)) > 0) {
+    read_buf[read_offset + bytes_read] = '\0';
+    read_offset = 0;
 
-    // TODO: maybe only reset when a newline is detected
-    read_buffer_offset = 0; /* reset read buffer offset immediately */
-
-    int vector_ops = (bytes_read / 16);
-    int remaining_bytes = bytes_read % 16;
     int copy_start = 0;
-
+    int vector_ops = (bytes_read / 16);
     int i = 0;
+
     while (vector_ops--) {
       uint8_t const* idx = (uint8_t const*) &read_buf[i];
       uint8x16_t vector = vld1q_u8(idx);
       uint8x16_t cmp = vceqq_u8(vector, newlines);
-
       uint8_t newlines_exists = vaddvq_u8(cmp);
 
       if (newlines_exists) {
-        int first_newline = extract_lowest_logical(cmp);
-        int newline_offset = i + first_newline;
-
-        read_buf[newline_offset] = '\0';
-
-        strncpy(word_buffer, &read_buf[copy_start], MAX_LENGTH - 1);
-        word_buffer[MAX_LENGTH - 1] = '\0';
-
-        printf("line: |%s|\n", word_buffer);
-
-        copy_start = newline_offset + 1;
-
-        /* redo, in case there are 2 newlines in the same vector*/
-        vector_ops++;
-        i-= 16;
+        for (int j = 0; j < 16; ++j) {
+          int offset = i + j;
+          if (read_buf[offset] == '\n') {
+            read_buf[offset] = '\0';
+            printf("%s\n", &read_buf[copy_start]);
+            copy_start = offset + 1;
+          }
+        }
       }
 
       i += 16;
     }
 
-    for (i = 0; i < remaining_bytes; ++i) {
+    for (; i < bytes_read; ++i) {
       if (read_buf[i] == '\n') {
         read_buf[i] = '\0';
-        strncpy(word_buffer, &read_buf[copy_start], MAX_LENGTH -1);
-        word_buffer[MAX_LENGTH - 1] = '\0';
+        printf("%s\n", &read_buf[copy_start]);
         copy_start = i + 1;
-        printf("line: |%s|\n", word_buffer);
       }
     }
 
-    if (copy_start < bytes_read) {
+    if (copy_start == 0) {
+      fprintf(stderr, "the buffer size exceeds the longest line, this is not supported\n");
+      return ELINES_TOO_LONG;
+    }
 
-      size_t remaining = bytes_read - copy_start;
-      memcpy(read_buf, &read_buf[copy_start], remaining);
-      read_buffer_offset = (int) remaining;
-
-      printf("bytes are left\n");
-      printf("copy start is at %d\n", copy_start);
-      printf("bytes read is at %ld\n", bytes_read);
-
-      // TODO: make sure that the last part of this buffer is not skipped.
-      // Copy last part of buffer to the first part of next buffer and set
-      // read buffer offset.
+    const size_t remainder = bytes_read - copy_start;
+    
+    if (remainder) {
+      memcpy(read_buf, &read_buf[copy_start], remainder);
+      read_offset = remainder;
     }
   };
-
 
   int status = close(fd);
   if (status == -1) {
     fprintf(stderr, "error closing the file\n");
-    return 1;
+    return EFILE_CLOSE;
   }
 }
